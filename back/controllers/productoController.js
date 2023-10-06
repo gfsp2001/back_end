95de3b980af377b4cd362061d5bbbe0428f5111a
gfsp2001 <gfsp2001@hotmail.com>
2023-10-06 09:01:45 -0500

    inventario productos, incrementar stock, giancarlo S.

--- back/controllers/productoController.js
+++ back/controllers/productoController.js
@@ -1,5 +1,13 @@
 var Producto = require('../models/producto');
+var Variedad = require('../models/variedad');
+var Inventario = require('../models/inventario');
+
 var fs = require('fs');
+var handlebars = require('handlebars');
+var ejs = require('ejs');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var path = require('path');
 
 const crear_producto_admin = async function (req, res) {
 
@@ -73,6 +81,261 @@ const obtener_datos_producto_admin = async function (req, res) {
     }
 }
 
+const actualizar_producto_admin = async function (req, res) {
+    if (req.user) {
+
+        let id = req.params['id'];
+        let data = req.body;
+        try {
+
+            let productos = await Producto.find({ titulo: data.titulo });
+
+            if (productos.length >= 1) {
+                if (productos[0]._id == id) {
+
+                    if (req.files) {
+                        // SI HAY IMAGEN
+                        var img_delete = productos[0].portada;
+                        var path_img_delete = './uploads/productos/' + img_delete;
+                        fs.unlink(path_img_delete, function (err) {
+                            if (err) throw new Error('No se pudo eliminar la imagen- ' + err);
+                        });
+
+                        var img_path = req.files.portada.path;
+                        var str_path = img_path.split('\\');
+                        var name = str_path[2];
+
+                        data.slug = data.titulo.trim().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+                        data.portada = name;
+
+                        let reg = await Producto.findByIdAndUpdate({ _id: id }, {
+                            titulo: data.titulo,
+                            slug: data.slug,
+                            descripcion: data.descripcion,
+                            portada: data.portada,
+                            categoria: data.categoria,
+                            tipo_variedad: data.tipo_variedad,
+                            tipo: data.tipo,
+                        });
+                        res.status(200).send({ data: reg });
+                    } else {
+                        //NO HAY IMAGEN
+                        data.slug = data.titulo.trim().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+                        let reg = await Producto.findByIdAndUpdate({ _id: id }, {
+                            titulo: data.titulo,
+                            slug: data.slug,
+                            descripcion: data.descripcion,
+                            categoria: data.categoria,
+                            tipo_variedad: data.tipo_variedad,
+                            tipo: data.tipo,
+                        });
+                        res.status(200).send({ data: reg });
+                    }
+
+                } else {
+                    res.status(200).send({ data: undefined, message: 'Ya existe un producto con ese titulo.' });
+                }
+
+            } else {
+                if (req.files) {
+                    // SI HAY IMAGEN
+                    var img_delete = productos[0].portada;
+                    var path_img_delete = './uploads/productos/' + img_delete;
+                    fs.unlink(path_img_delete, function (err) {
+                        if (err) throw new Error('No se pudo eliminar la imagen- ' + err);
+                    });
+
+                    var img_path = req.files.portada.path;
+                    var str_path = img_path.split('\\');
+                    var name = str_path[2];
+
+                    data.slug = data.titulo.trim().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+                    data.portada = name;
+
+                    let reg = await Producto.findByIdAndUpdate({ _id: id }, {
+                        titulo: data.titulo,
+                        slug: data.slug,
+                        descripcion: data.descripcion,
+                        portada: data.portada,
+                        categoria: data.categoria,
+                        tipo_variedad: data.tipo_variedad,
+                        tipo: data.tipo,
+                    });
+                    res.status(200).send({ data: reg });
+                } else {
+
+                    //NO HAY IMAGEN
+                    data.slug = data.titulo.trim().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+                    let reg = await Producto.findByIdAndUpdate({ _id: id }, {
+                        titulo: data.titulo,
+                        slug: data.slug,
+                        descripcion: data.descripcion,
+                        categoria: data.categoria,
+                        tipo_variedad: data.tipo_variedad,
+                        tipo: data.tipo,
+                    });
+                    res.status(200).send({ data: reg });
+                }
+            }
+
+        } catch (error) {
+            console.log(error);
+            res.status(403).send({ data: undefined, message: 'Ocurrio un problema al actualizar el producto.' });
+        }
+
+
+    } else {
+
+        res.status(403).send({ data: undefined, message: 'NoToken' });
+    }
+}
+
+const agregar_variedad_producto_admin = async function (req, res) {
+
+    if (req.user) {
+
+        let data = req.body;
+
+        let variedad = await Variedad.create(data);
+        res.status(200).send({ data: variedad });
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+}
+
+const obtener_variedades_producto_admin = async function (req, res) {
+
+    if (req.user) {
+        try {
+            let id = req.params.id;
+            let variedades = await Variedad.find({ producto: id });
+            res.status(200).send({ data: variedades });
+        } catch (error) {
+            console.log(error);
+            res.status(200).send({ data: undefined });
+        }
+
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+}
+
+const eliminar_variedad_producto_admin = async function (req, res) {
+
+    if (req.user) {
+        try {
+            let id = req.params.id;
+            let variedad = await Variedad.findById({ _id: id });
+            if (variedad.stock == 0) {
+                await Variedad.findByIdAndRemove({ _id: id });
+                res.status(200).send({ data: true });
+            } else {
+                res.status(200).send({ data: undefined, message: 'No puede eliminar la variedad.' });
+            }
+
+        } catch (error) {
+            console.log(error);
+            res.status(200).send({ data: undefined });
+        }
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+}
+
+const get_image_producto = async function (req, res) {
+    var img = req.params['img'];
+    fs.stat('./uploads/productos/' + img, function (err) {
+        if (!err) {
+            let path_img = './uploads/productos/' + img;
+            res.status(200).sendFile(path.resolve(path_img));
+        } else {
+            let path_img = './uploads/default.jpg';
+            res.status(200).sendFile(path.resolve(path_img));
+
+        }
+    });
+}
+
+const listar_productos_titulo_admin = async function (req, res) {
+
+    if (req.user) {
+
+        let productos = await Producto.find({ estado: true }).select('_id titulo').sort({ createdAt: -1 });
+        res.status(200).send({ data: productos });
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+
+}
+
+const registrar_inventario_admin = async function (req, res) {
+
+    if (req.user) {
+
+        let data = req.body;
+        //actualizar stock general
+        let inventario = await Inventario.create(data);
+        let producto = await Producto.findById({ _id: data.producto });
+        let nuevo_stock_producto = producto.stock + data.cantidad;
+        await Producto.findByIdAndUpdate({ _id: data.producto }, {
+            stock: nuevo_stock_producto
+        })
+        //actualizar stock variedad
+        let variedad = await Variedad.findById({ _id: data.variedad });
+        let nuevo_stock_variedad = variedad.stock + data.cantidad;
+        await Variedad.findByIdAndUpdate({ _id: data.variedad }, {
+            stock: nuevo_stock_variedad
+        })
+        // calcular margen de ganancia
+        let margen_ganancia = data.ganancia_producto;
+        let monto_ganancia = Math.round((data.costo_unidad * margen_ganancia) / 100);
+        let nuevo_precio_producto = data.costo_unidad + monto_ganancia;
+
+        if (producto.precio == 0) {
+            await Producto.findByIdAndUpdate({ _id: data.producto }, { precio: nuevo_precio_producto });
+        } else {
+            let ganancia_actual = producto.stock * data.costo_unidad;
+            console.log('ganancia_actual: ' + ganancia_actual);
+            let ganancia_nueva = data.cantidad * nuevo_precio_producto;
+            console.log('ganancia_nueva: ' + ganancia_nueva);
+            let total_stock = producto.stock + data.cantidad;
+            console.log('total_stock: ' + total_stock);
+            let total_ganancia = ganancia_actual + ganancia_nueva;
+            console.log('total_ganancia: ' + total_ganancia);
+            let nuevo_precio = Math.round(total_ganancia / total_stock);
+            console.log('nuevo_precio: ' + nuevo_precio);
+            await Producto.findByIdAndUpdate({ _id: data.producto }, { precio: nuevo_precio });
+        }
+        res.status(200).send({ data: inventario });
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+    }
+
+}
+
+const listar_inventario_admin = async function (req, res) {
+
+    if (req.user) {
+
+        let inventario = await Inventario.find().populate('producto').populate('variedad').sort({ createdAt: -1 });
+        res.status(200).send({ data: inventario });
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+
+}
+
 const cambiar_estado_producto_admin = async function (req, res) {
 
     if (req.user) {
@@ -94,10 +357,40 @@ const cambiar_estado_producto_admin = async function (req, res) {
     }
 }
 
+const obtener_inventario_admin = async function (req, res) {
+
+    if (req.user) {
+        try {
+            //gte: greater than equal -> mayor igual q 
+            let variedades = await Variedad.find({ stock: { $gte: 1 } }).select('_id sku titulo stock').populate({
+                path: 'producto',
+                select: '_id titulo categoria precio tipo'
+            });
+            res.status(200).send({ data: variedades });
+
+        } catch (error) {
+            res.status(200).send({ data: undefined });
+        }
+
+
+    } else {
+        res.status(404).send({ data: undefined, message: 'NoToken' });
+
+    }
+}
 
 module.exports = {
     crear_producto_admin,
     listar_productos_admin,
+    get_image_producto,
     obtener_datos_producto_admin,
+    actualizar_producto_admin,
+    agregar_variedad_producto_admin,
+    obtener_variedades_producto_admin,
+    eliminar_variedad_producto_admin,
+    listar_productos_titulo_admin,
+    registrar_inventario_admin,
+    listar_inventario_admin,
     cambiar_estado_producto_admin,
+    obtener_inventario_admin,
 }
--- back/routes/producto.js
+++ back/routes/producto.js
@@ -7,7 +7,16 @@ var app = express.Router();
 
 app.post('/crear_producto_admin', [auth.auth, path], productoController.crear_producto_admin);
 app.get('/listar_productos_admin', auth.auth, productoController.listar_productos_admin);
+app.get('/get_image_producto/:img', productoController.get_image_producto);
 app.get('/obtener_datos_producto_admin/:id', auth.auth, productoController.obtener_datos_producto_admin);
+app.put('/actualizar_producto_admin/:id', [auth.auth, path], productoController.actualizar_producto_admin);
+app.post('/agregar_variedad_producto_admin', auth.auth, productoController.agregar_variedad_producto_admin);
+app.get('/obtener_variedades_producto_admin/:id', auth.auth, productoController.obtener_variedades_producto_admin);
+app.delete('/eliminar_variedad_producto_admin/:id', auth.auth, productoController.eliminar_variedad_producto_admin);
+app.get('/listar_productos_titulo_admin', auth.auth, productoController.listar_productos_titulo_admin);
+app.post('/registrar_inventario_admin', auth.auth, productoController.registrar_inventario_admin);
+app.get('/listar_inventario_admin', auth.auth, productoController.listar_inventario_admin);
 app.put('/cambiar_estado_producto_admin/:id', auth.auth, productoController.cambiar_estado_producto_admin);
+app.get('/obtener_inventario_admin', auth.auth, productoController.obtener_inventario_admin);
 
 module.exports = app;
