5f0029876ab825950768ea703c7b18b1a98009f2
gfsp2001 <gfsp2001@hotmail.com>
2023-11-04 01:10:07 -0500

    ubicacion2

--- back/models/variedad.js
+++ back/models/variedad.js
@@ -5,6 +5,7 @@ var VariedadSchema = Schema({
     producto: { type: Schema.ObjectId, ref: 'producto', required: true },
     titulo: { type: String, required: true },
     sku: { type: String, required: true },
+    ubicacion: { type: String, required: true },
     stock: { type: Number, default: 0, required: true },
 
     createdAt: { type: Date, default: Date.now, required: true }
