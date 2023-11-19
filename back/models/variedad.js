893e882e0c2d3f65c90723ad67939f3455c86ca9
gfsp2001 <gfsp2001@hotmail.com>
2023-11-19 01:28:39 -0500

    cambios variedad

--- back/models/variedad.js
+++ back/models/variedad.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 var VariedadSchema = Schema({
     producto: { type: Schema.ObjectId, ref: 'producto', required: true },
     titulo: { type: String, required: true },
+    stockMinimo: { type: Number, required: true },
     sku: { type: String, required: true },
     ubicacion: { type: String, required: true },
     stock: { type: Number, default: 0, required: true },
